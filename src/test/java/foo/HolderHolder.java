43a5d51ad1ea9aa583a2a198444e993fc9c909d8
Ben Evans <benjamin.john.evans@gmail.com>
2015-07-01 12:22:05 +0100

    More tests

---
+++ src/test/java/foo/HolderHolder.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foo;
+
+/**
+ *
+ * @author boxcat
+ */
+public class HolderHolder {
+    private IntHolder myInt;
+    public StrHolder theString;
+}
